3b5f7ce14dbd9087f89c7451bfbdfa08d4e8aae6
Biray <games.biray@gmail.com>
2025-08-13 20:47:05 +0300

    added player class and circleshape

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import pygame
 # and the database_version variable
 # from database.py into the current file
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -14,15 +15,26 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
+    player_x = SCREEN_WIDTH / 2
+    player_y = SCREEN_HEIGHT / 2
+
+    player = Player(player_x, player_y)
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
+        # limit the fps to 60
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
